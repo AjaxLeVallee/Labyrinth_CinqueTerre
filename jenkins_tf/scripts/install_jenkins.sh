9a6302af2c318281a9ecfa5497e88228c9fd4da0
AjaxLevallee <aaron.anderson.ajax@gmail.com>
2021-11-12 18:07:03 -0500

    Incremental Update

--- jenkins_tf/scripts/install_jenkins.sh
+++ jenkins_tf/scripts/install_jenkins.sh
@@ -8,7 +8,7 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo amazon-linux-extras install epel
 sudo yum upgrade
-sudo yum install jenkins java-1.8.0-openjdk-devel -y
+sudo yum install jq jenkins java-1.8.0-openjdk-devel -y
 sudo systemctl daemon-reload
 sudo systemctl start jenkins
 sudo systemctl status jenkins
